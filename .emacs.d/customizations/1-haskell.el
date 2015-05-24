9bb714b24f666938f8c0aff1c005a482f0a3dfe0
Paul Schorfheide <pschorf2@gmail.com>
2015-05-23 22:13:57 -0500

    Add haskell customizations

---
+++ .emacs.d/customizations/1-haskell.el
@@ -0,0 +1,15 @@
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+(let ((cabal-path (expand-file-name "~/.cabal/bin")))
+  (setenv "PATH" (concat cabal-path ":" (getenv "PATH")))
+  (add-to-list 'exec-path cabal-path))
+
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
+(require 'company)
+(add-hook 'haskell-mode-hook 'company-mode)
+(add-to-list 'company-backends 'company-ghc)
+(custom-set-variables '(company-ghc-show-info t))
