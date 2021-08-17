931d54937579f59bd2baef7aef1183762c4ccf8f
Paul Schorfheide <paul.schorfheide@robinhood.com>
2021-08-16 20:14:26 -0500

    Set up metals for scala development

--- .emacs.d/programming.el
+++ .emacs.d/programming.el
@@ -5,9 +5,18 @@
   (setq lsp-keymap-prefix "C-c C-l")
   :hook ((lsp-mode . lsp-enable-which-key-integration)
 	 (haskell-mode . lsp)
-	 (literate-haskell-mode . lsp))
+	 (literate-haskell-mode . lsp)
+	 (scala-mode . lsp)
+	 (lsp-mode . lsp-lens-mode))
   :ensure t
-  :commands lsp)
+  :commands lsp
+  :config
+  (setq gc-cons-threshold 100000000) ;; 100mb
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq lsp-idle-delay 0.500)
+  (setq lsp-log-io nil)
+  (setq lsp-completion-provider :capf)
+  (setq lsp-prefer-flymake nil))
 
 (use-package python
   :mode ("\\.py\\'" . python-mode)
@@ -63,3 +72,22 @@
 
 (use-package vterm
   :ensure t)
+
+(use-package lsp-metals
+  :ensure t)
+(use-package lsp-ui
+  :ensure t
+  :config
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq lsp-ui-sideline-show-code-actions t))
+(use-package yasnippet
+  :ensure t)
+(use-package posframe
+  :ensure t)
+(use-package dap-mode
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode))
+
+(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
--- .emacs.d/source.el
+++ .emacs.d/source.el
@@ -1,5 +1,7 @@
 (use-package company
   :ensure t
+  :hook (scala-mode . company-mode)
   :config
   (add-hook 'after-init-hook 'global-company-mode)
+  (setq lsp-completion-provider :capf)
   (push 'company-capf company-backends))
