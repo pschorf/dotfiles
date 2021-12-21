f3c3efd75e425c077f0664b13bbd3dce73fecb45
Paul Schorfheide <paul@schorfheide.org>
2021-12-21 03:13:23 -0600

    stuff

--- .emacs.d/internet.el
+++ .emacs.d/internet.el
@@ -3,7 +3,7 @@
   :init
   (setq elfeed-feeds '(("ttrss+https://paul@rss.schorfheide.org/tt-rss" :use-authinfo t)))
   :bind (:map elfeed-search-mode-map
-	      ("U" . elfeed-update))
+	      ("U" . elfeed-protocol-ttrss-update))
   :commands elfeed)
 
 (use-package elfeed-protocol
@@ -25,34 +25,3 @@
 (setq erc-prompt-for-password nil)
 (setq erc-nick "pschorf")
 (setq erc-default-server "irc.libera.chat")
-
-  
-(use-package smudge
-  :ensure t)
-
-(defhydra hydra-spotify (:hint nil)
-    "
-^Search^                  ^Control^               ^Manage^
-^^^^^^^^-----------------------------------------------------------------
-_t_: Track               _SPC_: Play/Pause        _+_: Volume up
-_m_: My Playlists        _n_  : Next Track        _-_: Volume down
-_f_: Featured Playlists  _p_  : Previous Track    _x_: Mute
-_u_: User Playlists      _r_  : Repeat            _d_: Device
-^^                       _s_  : Shuffle           _q_: Quit
-"
-    ("t" smudge-track-search :exit t)
-    ("m" smudge-my-playlists :exit t)
-    ("f" smudge-featured-playlists :exit t)
-    ("u" smudge-user-playlists :exit t)
-    ("SPC" smudge-controller-toggle-play :exit nil)
-    ("n" smudge-controller-next-track :exit nil)
-    ("p" smudge-controller-previous-track :exit nil)
-    ("r" smudge-controller-toggle-repeat :exit nil)
-    ("s" smudge-controller-toggle-shuffle :exit nil)
-    ("+" smudge-controller-volume-up :exit nil)
-    ("-" smudge-controller-volume-down :exit nil)
-    ("x" smudge-controller-volume-mute-unmute :exit nil)
-    ("d" smudge-select-device :exit nil)
-    ("q" quit-window "quit" :color blue))
-
-(bind-key "C-c C-. a" #'hydra-spotify/body)
--- .emacs.d/mail.el
+++ .emacs.d/mail.el
@@ -1,50 +1,36 @@
-  (when (require 'mu4e nil 'noerror)
-
-    (setq mu4e-maildir (expand-file-name "~/Maildir"))
-    (setq mu4e-contexts
-          `( ,(make-mu4e-context
-               :name "Gmail"
-               :vars '((mu4e-drafts-folder . "/Gmail/[Gmail].Drafts")
-                       (mu4e-sent-folder . "/Gmail/[Gmail].Sent Mail")
-                       (mu4e-trash-folder . "/Gmail/[Gmail].Trash")
-                       (mu4e-maildir-shortcuts . (("/Gmail/INBOX" . ?i)
-                                                   ("/Gmail/[Gmail].Sent Mail" . ?s)
-                                                   ("/Gmail/[Gmail].Trash" . ?t)))
-                       (user-mail-address . "pschorf2@gmail.com")))
-
-              ,(make-mu4e-context
-               :name "Schorfheide"
-               :vars '((mu4e-drafts-folder . "/Schorfheide/Drafts")
-                       (mu4e-sent-folder . "/Schorfheide/Sent")
-		       (mu4e-trash-folder . "/Schorfheide/Trash")
-                       (mu4e-maildir-shortcuts . (("/Schorfheide/INBOX" . ?i)
-                                                   ("/Schorfheide/Sent" . ?s)
-                                                   ("/Schorfheide/Trash" . ?s)))
-                       (user-mail-address . "paul@schorfheide.org")))))
-
-    (setq mu4e-get-mail-command "offlineimap")
-    (require 'smtpmail)
-    (setq user-full-name "Paul Schorfheide")
-    (setq mu4e-attachment-dir "~/Downloads")
-    (setq mu4e-sent-messages-behavior (lambda ()
-                                        (if (string= (message-sendmail-envelope-from) "pschorf2@gmail.com")
-                                            'delete 'sent)))
-    (setq message-send-mail-function 'smtpmail-send-it
-          starttls-use-gnutls t
-          smtpmail-starttls-credentials
-          '(("mail.schorfheide.org" 2525 nil nil))
-          smtpmail-auth-credentials
-          (expand-file-name "~/.authinfo.gpg")
-          smtpmail-default-smtp-server "mail.schorfheide.org"
-          smtpmail-smtp-server "mail.schorfheide.org"
-          smtpmail-smtp-service 2525
-          smtpmail-debug-info t)
-    (defun mu4e-maybe-fetch-mail (prefix)
-      (interactive "P")
-      (if prefix
-          (mu4e-update-mail-and-index nil)
-        (mu4e-update-index)))
-    (define-key mu4e-main-mode-map (kbd "U") 'mu4e-maybe-fetch-mail))
+(when (require 'mu4e nil 'noerror)
+  
+  (setq mu4e-maildir (expand-file-name "~/Maildir"))
+  (setq mu4e-drafts-folder "/Drafts")
+  (setq mu4e-sent-folder "/Sent")
+  (setq mu4e-trash-folder "/Trash")
+  (setq mu4e-maildir-shortcuts '(("/INBOX" . ?i)
+				 ("/Sent" . ?s)
+				 ("/Trash" . ?t)))
+  (setq user-mail-address "paul@schorfheide.org")
+  
+  (setq mu4e-get-mail-command "offlineimap")
+  (require 'smtpmail)
+  (setq user-full-name "Paul Schorfheide")
+  (setq mu4e-attachment-dir "~/Downloads")
+  (setq mu4e-sent-messages-behavior (lambda ()
+                                      (if (string= (message-sendmail-envelope-from) "pschorf2@gmail.com")
+                                          'delete 'sent)))
+  (setq message-send-mail-function 'smtpmail-send-it
+        starttls-use-gnutls t
+        smtpmail-starttls-credentials
+        '(("mail.schorfheide.org" 2525 nil nil))
+        smtpmail-auth-credentials
+        (expand-file-name "~/.authinfo.gpg")
+        smtpmail-default-smtp-server "mail.schorfheide.org"
+        smtpmail-smtp-server "mail.schorfheide.org"
+        smtpmail-smtp-service 2525
+        smtpmail-debug-info t)
+  (defun mu4e-maybe-fetch-mail (prefix)
+    (interactive "P")
+    (if prefix
+        (mu4e-update-mail-and-index nil)
+      (mu4e-update-index))))
 
 (setq mml-smime-use 'epg)
 (setq mml-default-encrypt-method "smime")
